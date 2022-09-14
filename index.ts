a8122bb87aaf49713cc154cdd9f25db347896a0f
ocaiomeira <107116396+ocaiomeira@users.noreply.github.com>
2022-09-14 15:16:35 -0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('Caio');
+let amor = ['Caio', 'Karol'];
+amor.push('Amores');
+console.log(amor);
+appDiv.innerHTML = `<h1>KAROL = AMOR DA MINHA VIDA</h1>`;
+
